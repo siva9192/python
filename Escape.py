142d730d504b23148617f547ab0afa9d41b309fc
siva9192 <barinepallisiva@gmail.com>
2022-05-21 18:56:15 +0530

    added Escape code with examples

---
+++ Escape.py
@@ -0,0 +1,44 @@
+#Escape
+"""
+\'	    Single Quote	
+\\	    Backslash	
+\n	    New Line	
+\r	    Carriage Return	
+\t	    Tab	
+\b	    Backspace	
+\f	    Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+"""
+
+#Singe Quote
+name ="\"python\""
+print(name)
+
+#Backslash
+name= "python\\"
+print(name)
+
+#New Line
+name="first\nlast"
+print(name)
+
+#Carriage Return	
+name="first\rlast"
+print(name)
+
+# Tab
+name="first\tlast"
+print(name)
+
+# Backspace
+name="firstlas\bt"
+print(name)
+
+#Octal value	
+name = "\110\145\154\154\157"
+print(name) 
+
+#Hex value
+name = "\x48\x65\x6c\x6c\x6f"
+print(name) 
