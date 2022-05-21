0070357fb16a5cfd5babf36b756f5dcfdd787b44
siva9192 <barinepallisiva@gmail.com>
2022-05-21 18:19:09 +0530

    added code for format function

---
+++ Functions.py
@@ -0,0 +1,14 @@
+#Functions
+"""
+1.Format
+
+"""
+# 1.Format
+# it is used to concat diffrent type of data
+Name= 'Siva'
+Phone= 9866525117
+
+print("I'm {} and my Number is {}".format(Name,Phone)) 
+#OR
+print("I'm {0} and my Number is {1}".format(Name,Phone))
+
