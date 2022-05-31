a00fbdf9f1b09d84d5bc4933fef22a2d78d9abe6
siva9192 <barinepallisiva@gmail.com>
2022-05-31 15:51:08 +0530

    Added while statement

---
+++ While.py
@@ -0,0 +1,19 @@
+i=1
+while i<100:
+    print(i)
+    i += 1
+
+#break
+i=1
+while i<10:
+    print(i)
+    i +=1
+    if i== 5:
+        break
+#continue
+i=1
+while i<10:
+    i +=1
+    if i == 5:
+        continue
+    print(i)
